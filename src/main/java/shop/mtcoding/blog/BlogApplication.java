247d6b4207312d7a2a4fa37ab01588ee56d180f8
jaybon1 <ponggas2@gmail.com>
2023-07-25 15:51:12 +0900

    세팅완료

---
+++ src/main/java/shop/mtcoding/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package shop.mtcoding.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		System.out.println("안녕");
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/shop/mtcoding/blog/controller/HelloController.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
