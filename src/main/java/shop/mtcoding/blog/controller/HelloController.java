a1ecb8e0a5e615490c2f42a26b6449f25a189767
jaybon1 <ponggas2@gmail.com>
2023-07-25 16:50:33 +0900

    머스태치공부

--- src/main/java/shop/mtcoding/blog/controller/HelloController.java
+++ src/main/java/shop/mtcoding/blog/controller/HelloController.java
@@ -1,13 +1,51 @@
 package shop.mtcoding.blog.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Controller
 public class HelloController {
-    
+
     @GetMapping("/test")
-    public String test(){
+    public String test(Model model) {
+        model.addAttribute("username", "ssar");
         return "test";
+    }
+
+    @GetMapping("/array")
+    public String test2(Model model) {
+        List<String> list = new ArrayList<>();
+        list.add("바나나");
+        list.add("딸기");
+        model.addAttribute("list", list);
+        return "array";
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class User {
+        private int id;
+        private String username;
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User(1, "ssar"));
+        list.add(new User(2, "cos"));
+        model.addAttribute("list", list);
+        model.addAttribute("yougood", true);
+        return "users";
     }
 }
