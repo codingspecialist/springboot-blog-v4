54246119e8016b126b3c448baf7998dc1650bbbb
jaybon1 <ponggas2@gmail.com>
2023-07-26 15:50:40 +0900

    화면 구현 완료

--- src/main/java/shop/mtcoding/blog/BlogApplication.java
+++ src/main/java/shop/mtcoding/blog/BlogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
 	public static void main(String[] args) {
-		System.out.println("안녕");
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
